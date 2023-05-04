ce1ca125ec462b7f19c4aab8c712b75ecaac7e25
Rafael Fernandes <marcorafaeldemelo16@gmail.com>
2023-05-04 12:19:01 -0300

    corrigido problema de secure storage

--- GPTApp/menu.xaml.cs
+++ GPTApp/menu.xaml.cs
@@ -10,8 +10,9 @@ public partial class menu : ContentPage
 
     private async void OnDefineTokenClicked(object sender, EventArgs e)
     {
+        string TokenUserApi = await SecureStorage.Default.GetAsync("token-api-user");
         // Mostra um alerta para o usuário com um campo de edição e botões OK e Cancelar
-        string Tokem = await DisplayPromptAsync("Token da API", "Digite o token da API");
+        string Tokem = await DisplayPromptAsync("Token da API", "Digite o token da API", initialValue: TokenUserApi);
 
         if (!string.IsNullOrEmpty(Tokem))
         {
