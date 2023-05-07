411e23e931d747ed92c0f8e73571af6477fe498a
Rafael Fernandes <marcorafaeldemelo16@gmail.com>
2023-05-07 15:51:04 -0300

    removendo plataformas

--- GPTApp/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace GPTApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
